569902b1399adc2b7babe659d8c171b96eb9d1c3
jamiahuswalton <jamiahuswalton@gmail.com>
2018-07-12 20:39:19 -0500

    Ignore Database file

--- Assets/_Scripts/DatabasePassword.cs
+++ Assets/_Scripts/DatabasePassword.cs
@@ -6,7 +6,7 @@ public class DatabasePassword : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		//This file will be ignored in Github
 	}
 	
 	// Update is called once per frame
